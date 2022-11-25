8aa0ad9f764a6e3042a32fcf7cabbd4a08962d19
divyadyadey <divyadyadey@gmail.com>
2022-11-25 12:55:46 +0530

    demo for team

---
+++ src/main/java/com/mavenweb/Divya/App.java
@@ -0,0 +1,13 @@
+package com.mavenweb.Divya;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is SE lab internal 1" );
+    }
+}
